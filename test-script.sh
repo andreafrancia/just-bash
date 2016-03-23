d380de6f0440b48deb4a54a3d6d1a8a1d226d65f
Andrea Francia <andrea@andreafrancia.it>
2016-03-23 08:57:27 +0100

    Unit testing prototype

--- test-script.sh
+++ test-script.sh
@@ -1,5 +1,12 @@
 set -o errexit
-script/deploy
-diff -u <(script/deploy) - << EOF
-Linux precise32 3.2.0-23-generic-pae #36-Ubuntu SMP Tue Apr 10 22:19:09 UTC 2012 i686 i686 i386 GNU/Linux
-EOF
+assert_no_failures=true
+expect_success() {
+    ( "$@"; ) || {
+        local actual_exit_code="$?"
+        echo "Failed expectation for command: \`$*'"
+        echo " - expected success (exit code == 0) got failure (exit code == $actual_exit_code)"
+        assert_no_failures=false
+    }
+}
+expect_success false
+$assert_no_failures
